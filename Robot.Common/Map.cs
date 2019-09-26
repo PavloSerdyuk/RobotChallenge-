2593adb7c07277fbbd19f94ef8be5c482a537461
p.serdyuk <Pavlo.Serdyuk@kryonsystems.com>
2019-09-26 09:36:20 +0300

    Formatting, fix issue of station generation - they could be at one place.

--- Robot.Common/Map.cs
+++ Robot.Common/Map.cs
@@ -33,24 +33,24 @@ namespace Robot.Common
                     for (int j = -distance; j <= distance; j++)
                     {
                         var newPos = new Position(nearPosition.X + i, nearPosition.Y + j);
-                        if (! IsValid(newPos))
+                        if (!IsValid(newPos))
                             continue;
 
-                        
-                        if (! robots.Any(r=>r.Position==newPos))
+
+                        if (!robots.Any(r => r.Position == newPos))
                             return newPos;
 
                     }
                 distance++;
             }
             throw new Exception("All cells are filled");
-            
+
         }
 
 
         private readonly Position _maxPozitionForSmallMap = new Position() { X = 100, Y = 100 };
-        
-        public Position MinPozition = new Position(){X=0,Y=0};
+
+        public Position MinPozition = new Position() { X = 0, Y = 0 };
         public Position MaxPozition;
 
         public IList<EnergyStation> Stations = new List<EnergyStation>();
@@ -68,18 +68,18 @@ namespace Robot.Common
         /// <returns></returns>
         public List<EnergyStation> GetNearbyResources(Position pozition, int distance)
         {
-            return Stations.Where(station => (Math.Abs(station.Position.X - pozition.X) <=  distance)
-                && (Math.Abs(station.Position.Y - pozition.Y) <= distance) ).ToList();
+            return Stations.Where(station => (Math.Abs(station.Position.X - pozition.X) <= distance)
+                && (Math.Abs(station.Position.Y - pozition.Y) <= distance)).ToList();
         }
 
         private List<EnergyStation> CopyResources()
         {
-            return Stations.Select(energyResource => new EnergyStation() {Energy = energyResource.Energy, Position = energyResource.Position.Copy(), RecoveryRate = energyResource.RecoveryRate}).ToList();
+            return Stations.Select(energyResource => new EnergyStation() { Energy = energyResource.Energy, Position = energyResource.Position.Copy(), RecoveryRate = energyResource.RecoveryRate }).ToList();
         }
 
         public Map Copy()
         {
-            return new Map(){MaxPozition = _maxPozitionForSmallMap, Stations = CopyResources()};
+            return new Map() { MaxPozition = _maxPozitionForSmallMap, Stations = CopyResources() };
         }
 
 
@@ -90,31 +90,37 @@ namespace Robot.Common
 
         public Map(int variant, int initialRobotCount)
         {
-           
-            
+
+
             MaxPozition = _maxPozitionForSmallMap;
 
             Variant.Initialize(variant);
             var mapParams = Variant.GetInstance();
 
-            var numberOfResources = (int)(initialRobotCount*mapParams.EnergyStationForAttendant);
+            var numberOfResources = (int)(initialRobotCount * mapParams.EnergyStationForAttendant);
 
             var listOfUsed = new List<Position>();
             Random r = new Random();
             Position newPosition = null;
+
+
+
             for (int i = 0; i < numberOfResources; i++)
             {
+
                 do
                 {
-                    newPosition = new Position() {X = r.Next(MaxPozition.X), Y = r.Next(MaxPozition.Y)};
-                } while ( listOfUsed.Contains(newPosition));
 
+                    newPosition = new Position() { X = r.Next(MaxPozition.X), Y = r.Next(MaxPozition.Y) };
 
+                } while (listOfUsed.Contains(newPosition));
+                //So station could not be at the same place
+                listOfUsed.Add(newPosition);
                 var minGrowth = mapParams.MinEnergyGrowth;
                 var maxGrowth = mapParams.MaxEnergyGrowth;
 
- 
-                Stations.Add(new EnergyStation() { RecoveryRate = minGrowth + r.Next(maxGrowth - minGrowth), Energy = 0, Position = newPosition});
+
+                Stations.Add(new EnergyStation() { RecoveryRate = minGrowth + r.Next(maxGrowth - minGrowth), Energy = 0, Position = newPosition });
             }
 
         }
